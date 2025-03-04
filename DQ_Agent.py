f16471bdfff1057e7835fc4a8c338a6b3512330c
Ben Kurzion <bxk389@case.edu>
2025-03-04 17:17:26 -0500

    initial work done

--- DQ_Agent.py
+++ DQ_Agent.py
@@ -7,14 +7,15 @@ from model import DQN
 import numpy as np
 from Game import Game
 import random
+from Map import Map
 
 
 class State:
     '''
     A game state at some time point t is defined as a single vector (1 x (3*52))
-        - hand the agent currently holds
-        - cards that the agent as seen in play
-        - the next card that the agent intends to play (the action)
+        - hand the agent currently holds before playing the trick
+        - cards that the agent as seen in play before playing its own card
+        - the next card that the agent intends to play in this trick
     All will be represented as a one hot encoding
     '''
     def __init__(self, hand : np.ndarray, cards_seen : np.ndarray, next_action : np.ndarray):
@@ -30,31 +31,49 @@ class Memory:
     A memory is a single time point in the replay memory. A training memory needs
         - current State
         - reward recived for taking the action (as stored in the state)
-        - next state (as a result of taking the action)
-        - a flag denoting whether or not the current state is a terminal state
     If the current state is terminal (terminal_flag=True), then next_state=Null
     '''
-    def __init__(self, current_state : State, reward : float, next_state : State, terminal_flag : bool):
+    def __init__(self, current_state : State, reward : float):
         self.current_state = current_state
         self.reward = reward
-        self.next_state = next_state
-        self.terminal_flag = terminal_flag
 
 
 def generate_training_data(model : DQN) -> list:
     '''
     Play a bunch of games and store all of the model experiences as a list of memories
     '''
+    replay_memory = []
+    map = Map()
+
     for simulated_game in range (5000):
         game = Game(["Agent", "P1", "P2", "P3"])
+        cards_seen = np.zeros(shape=(1,52))
+
         # Loop for the entire game
         while len(game.current_player.hand) > 0:
             inital_score = game.players[0].score
-
+            current_trick_cards = []
             if game.current_player.name == 'Agent':
+                # Note the agent's hand before playing the card
+                hand = np.zeros(shape=(1,52))
+                for card in game.players[0].hand:
+                    index = map.get_index(rank=card.rank, suit=card.suit)
+                    hand[index] = 1
+
+
                 # Run the network to get a next action --> next card to play
                 # Translate the network output to rank and suit
-                game.play_card(game.deck.get_card(rank="", suit=""))
+                # card = agent.chosen_card
+                game.play_card(game.deck.get_card(rank=card.rank, suit=card.suit))
+
+
+                # Mark the played card as the next_action
+                index = map.get_index(rank=card.rank, suit=card.suit)
+                next_state = np.zeros(shape=(1,52))
+                next_state[index] = 1
+
+                # Add played card to current trick 
+                current_trick_cards.append(card)
             else:
                 # Get a random card and submit to game object
                 rand_card_index = random.randint(0, len(game.current_player.hand))
@@ -64,10 +83,23 @@ def generate_training_data(model : DQN) -> list:
                     rand_card = game.current_player.hand[rand_card_index]
                 game.play_card(rand_card)
 
+                # Mark the played card as having been played in this trick
+                current_trick_cards.append(card)
+
             # Check if the trick is over and create a Memory
             if len(game.trick) == 0:
+                # Calculate the necessary information for memory
                 reward = game.players[0].score - inital_score
-        game.play_card()
+                current_state = State(hand=hand, cards_seen=cards_seen, next_action=next_state)
+                memory = Memory(current_state, reward)
+                replay_memory.append(memory)
+
+                # Update the cards seen with all the cards played in the terminated trick
+                for card in current_trick_cards:
+                    index = map.get_index(rank=rand_card.rank, suit=rand_card.suit)
+                    current_state[index] = 1
+
+                
         
 
 
---
+++ Map.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+class Map:
+    def __init__(self):
+        self.dict = {
+            1 : ['2' 'Hearts'],
+            2 : ['3', 'Hearts'],
+            3 : ['4', 'Hearts'],
+            4 : ['5', 'Hearts'],
+            5 : ['6', 'Hearts'],
+            6 : ['7', 'Hearts'],
+            7 : ['8', 'Hearts'],
+            8 : ['9', 'Hearts'],
+            9 : ['10', 'Hearts'],
+            10 : ['J', 'Hearts'],
+            11 : ['Q', 'Hearts'],
+            12 : ['K', 'Hearts'],
+            13 : ['A', 'Hearts'],
+            14 : ['2' 'Diamonds'],
+            15 : ['3', 'Diamonds'],
+            16 : ['4', 'Diamonds'],
+            17 : ['5', 'Diamonds'],
+            18 : ['6', 'Diamonds'],
+            19 : ['7', 'Diamonds'],
+            20 : ['8', 'Diamonds'],
+            21 : ['9', 'Diamonds'],
+            22 : ['10', 'Diamonds'],
+            23 : ['J', 'Diamonds'],
+            24 : ['Q', 'Diamonds'],
+            25 : ['K', 'Diamonds'],
+            26 : ['A', 'Diamonds'],
+            27 : ['2' 'Clubs'],
+            28 : ['3', 'Clubs'],
+            29 : ['4', 'Clubs'],
+            30 : ['5', 'Clubs'],
+            31 : ['6', 'Clubs'],
+            32 : ['7', 'Clubs'],
+            33 : ['8', 'Clubs'],
+            34 : ['9', 'Clubs'],
+            35 : ['10', 'Clubs'],
+            36 : ['J', 'Clubs'],
+            37 : ['Q', 'Clubs'],
+            38 : ['K', 'Clubs'],
+            39 : ['A', 'Clubs'],
+            40 : ['2' 'Spades'],
+            41 : ['3', 'Spades'],
+            42 : ['4', 'Spades'],
+            43 : ['5', 'Spades'],
+            44 : ['6', 'Spades'],
+            45 : ['7', 'Spades'],
+            46 : ['8', 'Spades'],
+            47 : ['9', 'Spades'],
+            48 : ['10', 'Spades'],
+            49 : ['J', 'Spades'],
+            50 : ['Q', 'Spades'],
+            51 : ['K', 'Spades'],
+            52 : ['A', 'Spades'],
+        }
+
+    def get_index(self, rank : str, suit : str):
+        for k,v, in self.dict.items():
+            if rank == v[0] and suit == v[1]:
+                return k - 1
+
