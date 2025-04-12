45a5f0358b80157884e0da658567061c3ddaf053
Gabriel Wolf <gabeericwolf@gmail.com>
2025-04-12 15:01:07 -0400

    green and name at bottom

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 game = Game(["Rachel", "Meal", "Shraf", "Simi"], 100, True)
 
+def emit_game_state():
+    """Emit the current game state to all clients."""
+    trick_cards = [Game.dict_repr(trick[1]) for trick in game.trick]
+    emit('update_cards', {
+        "player_cards": Game.dict_repr(game.current_player.hand),
+        "center_cards": trick_cards,
+        "player_name": game.current_player.name,
+        "player_score": game.current_player.score
+    }, broadcast=True)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,28 +27,27 @@ def index():
 @socketio.on('connect')
 def send_initial_cards():
     """Send player cards and center cards on connection."""
-    trick_cards = [Game.dict_repr(trick[1]) for trick in game.trick]
-    emit('update_cards', {"player_cards": Game.dict_repr(game.current_player.hand), "center_cards": trick_cards})
+    emit_game_state()
 
 @socketio.on('play_card')
 def play_card(card):
     """Move a played card from player cards to the center."""
     global game
     card = game.deck.get_card(card['rank'], card['suit'])
-    print(card)
-    print(game.current_player.hand)
+    print(game.current_player.name)
+    print(game.current_player.score)
+    # print(card)
+    # print(game.current_player.hand)
     if card in game.current_player.hand:
         game.play_card(card)
-        trick_cards = [Game.dict_repr(trick[1]) for trick in game.trick]
-        emit('update_cards', {"player_cards": Game.dict_repr(game.current_player.hand), "center_cards": trick_cards}, broadcast=True)
+        emit_game_state()
 
 @socketio.on('get_new_cards')
 def get_new_cards():
     """Generates a new game."""
     global game
     game = Game(["Rachel", "Meal", "Shraf", "Simi"], 100)
-    trick_cards = [Game.dict_repr(trick[1]) for trick in game.trick]
-    emit('update_cards', {"player_cards": Game.dict_repr(game.current_player.hand), "center_cards": trick_cards}, broadcast=True)
+    emit_game_state()
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
